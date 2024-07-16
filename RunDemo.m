1c92bdad0d7998c7d0e5113041bbdb911451771e
Neda Darbeheshti <Neda.Darbeheshti@ga.gov.au>
2024-07-16 17:47:53 +0100

    news

--- RunDemo.m
+++ RunDemo.m
@@ -1,6 +1,6 @@
-%% Gravimetric quasigeoid estimation
+% Gravimetric quasigeoid estimation
 %
-% RunGeoidLSC is the main m-file preforming the gravimetric quasiGeoid estimation.
+% RunDemo is the main m-file preforming the gravimetric quasiGeoid estimation.
 %
 % Main parameters to be defined:
 %                      - Grid/Tiling Parameters
@@ -17,7 +17,7 @@
 % Main functions:
 %             - ImportAndFormatData: import all different data sets needed for LSC.
 %             - computeTerrainEffect: calculates the Residual Terrain Model.
-%             - ComputeLSC: runs the LSC (Least Squares Collocation) in blocks.
+%             - computeGravimetryLSC: runs the LSC (Least Squares Collocation) in blocks.
 %             - MosaicTiles: mosaic the tiles with weights and save final result in tif 
 %
 % Written by Jack McCubbine
@@ -44,33 +44,30 @@ GRID_PARA.MAXLONG=142;
 GRID_PARA.MINLAT=-32.5;
 GRID_PARA.MAXLAT=-32;
 %% DEM data - N.B. the dem is used to specify the grid nodes.
-DEM_PARA.filename='Data\DEM\AUSDEM1m.xyz';
+DEM_PARA.filename='Data\DEM\AUSDEM1min.xyz';
 DEM_PARA.num_cols=4861;
 DEM_PARA.num_rows=3181;
 %% Gravity data
-% First run ./Data/GRAVITY/XXXX/PrepareGravity_XXXXX.m
-% And then /Data/GRAVITY/Combine_Gravity_Data.m
 % this collates all of the gravity and position data into one matlab array.
-GRAV_PARA.filename='Data\processedData\GravityAllVicNSW.mat';
+GRAV_PARA.filename='Data\processedData\Terrestrial_Gravity.mat';
 GRAV_PARA.filename1=[];%'Data\GRAVITY\Xcalibur_Gravity.mat';
 GRAV_PARA.TypeB=1;% This is a Type B uncertainty value (in mGal) which is added to the uncertainty values.
 GRAV_PARA.Grav_Faye_TypeB=3;
 %% Gravity Gradiometry data
-% Add notes here
 GRAV_GRAD_PARA.filename='Data\GRAVITY_GRAD\Xcalibur_FVD_GDD.mat';
 GRAV_GRAD_PARA.TypeB=10^(-5);% This is a Type B uncertainty value (in mGal/m) which is added to the uncertainty values.
-GRAV_GRAD_PARA.avail=true;
+GRAV_GRAD_PARA.avail=false;
 %% Covariance function parameters
 COV_PARA.Compute_Empircal_COV_Dec=3; % Decimation factor for empirical covariance estimates. e.g. 1 is no decimation, 2 drops 50% of the data etc. see sph_empcov for logic.
 COV_PARA.Fit_Empircal_COV='auto';%'auto';% process to fit covariance N & M function values 'man' for manual to fit them on the cmd line or 'auto' , '' to just use what you supply here.
-COV_PARA.FitEmpiricalCOVNSearch=[21600,1,21600];%10800 is not working:speckelly pattern% Start, step, stop parameter sweep values for N parameter - if auto
+COV_PARA.FitEmpiricalCOVNSearch=[21600,1,21600];% Start, step, stop parameter sweep values for N parameter - if auto
 COV_PARA.FitEmpiricalCOVMSearch=[200,20,300];% Start, step, stop parameter sweep values for M parameter - if auto
 COV_PARA.N=10800;% max Legender polynonial of cov func. 
 COV_PARA.M=200;% min Legender polynonial of cov func. 
 COV_PARA.width=3;% Size of precomputed cov function in degrees - must be larger the the distance between any two points on a tile. 
 COV_PARA.res=30/3600; % Resolution of the covariance function
 COV_PARA.COV_COMPUTED_Tilewise=true;% This recomputes the covariance function for each tile.
-COV_PARA.Airbornedataonly=false;%Only use airborne data in establishing Covariance parameters - good to use if we are using EGM2008 as the references as terrestrial data are not independent.
+COV_PARA.Airbornedataonly=false;% Only use airborne data in establishing Covariance parameters - good to use if we are using EGM2008 as the references as terrestrial data are not independent.
 COV_PARA.COVPlot=false;% true plots progress, false turns this off.
 %% Topo condensation parameters
 Topo_PARA.Corr=true;% MAKE SURE YOU TURN THIS ON!!!
@@ -93,29 +90,29 @@ LEVELLING_PARA.Compare_To_Existing_Model=true;% If true, the levelling data are 
 LEVELLING_PARA.Existing_Model='Data\EXISTING_GEOID_MODELS\AGQG20221120.mat';% File location of the existing model.
 LEVELLING_PARA.max_diff=0.15;% Threshold for an outlier with the GNSS-levelling
 %% Output
-OUTPUT_PARA.Grids_name='D:/GAbackup/Outputs/Grids/';
-OUTPUT_PARA.Tiles_dir_name='D:/GAbackup/Outputs/ResidualTiles/';
+OUTPUT_PARA.Grids_name='outputs/Grids/';
+OUTPUT_PARA.Tiles_dir_name='outputs/ResidualTiles/';
 OUTPUT_PARA.PLOT_GRIDS=true;% A gridded solution is plotted and output as well as the tiles.
-OUTPUT_PARA.plotsFolder=['D:/GAbackup/Outputs/plots/',date,'demo'];
+OUTPUT_PARA.plotsFolder=['outputs/plots/',date,'demo'];
 
 % Keep the computer awake
 keepawake=true;% Setting this to true wiggles the mouse every so often so the compute doesnt go to sleep.
 %% Run the LSC code
 
-% disp('1/4 ..........................importAndFormatData is running ')
-% [Gravo,gravGradFiltered,DEM_data,ZDEM_griddedInterpolant,LongDEM,LatDEM,...
-%  GGM_Gravity_griddedInterpolant,GGM_Zeta_griddedInterpolant,Lev,...
-%  REFERENCE_Zeta_griddedInterpolant,GRID_REF,Coastline]=importAndFormatData...
-%  (GRID_PARA,DEM_PARA,GRAV_PARA,Topo_PARA,COAST_PARA,LEVELLING_PARA,GGM_PARA,GRAV_GRAD_PARA);
+disp('1/4 ..........................importAndFormatData is running ')
+[Gravo,gravGradFiltered,DEM_data,ZDEM_griddedInterpolant,LongDEM,LatDEM,...
+ GGM_Gravity_griddedInterpolant,GGM_Zeta_griddedInterpolant,Lev,...
+ REFERENCE_Zeta_griddedInterpolant,GRID_REF,Coastline]=importAndFormatData...
+ (GRID_PARA,DEM_PARA,GRAV_PARA,Topo_PARA,COAST_PARA,LEVELLING_PARA,GGM_PARA,GRAV_GRAD_PARA);
 
-% save([OUTPUT_PARA.Grids_name,'oneTileData',date,'.mat'],...
-%  'Gravo','gravGradFiltered','DEM_data','LongDEM','LatDEM','Lev','GRID_REF','Coastline')
-% 
-% save([OUTPUT_PARA.Grids_name,'oneTileGriddedInterpolant',date,'.mat'],...
-% 'ZDEM_griddedInterpolant','GGM_Gravity_griddedInterpolant','GGM_Zeta_griddedInterpolant','REFERENCE_Zeta_griddedInterpolant')
+save([OUTPUT_PARA.Grids_name,'oneTileData141-32','.mat'],...
+ 'Gravo','DEM_data','LongDEM','LatDEM','Lev','GRID_REF','Coastline')
 
-dataDemo=importdata(['D:/GAbackup/Data/Demo/','oneTileData05-Jun-2024.mat']);
-interpolantDemo=importdata(['D:/GAbackup/Data/Demo/','oneTileGriddedInterpolant05-Jun-2024.mat']);
+save([OUTPUT_PARA.Grids_name,'oneTileGriddedInterpolant141-32','.mat'],...
+'ZDEM_griddedInterpolant','GGM_Gravity_griddedInterpolant','GGM_Zeta_griddedInterpolant','REFERENCE_Zeta_griddedInterpolant')
+
+% dataDemo=importdata(['D:/GAbackup/Data/Demo/','oneTileData05-Jun-2024.mat']);
+% interpolantDemo=importdata(['D:/GAbackup/Data/Demo/','oneTileGriddedInterpolant05-Jun-2024.mat']);
 
 % Plot input data: 
 
