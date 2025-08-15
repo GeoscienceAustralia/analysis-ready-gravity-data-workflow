44c1777b93a3a51ec04141d7ddcef4aca449131a
Neda Darbeheshti <Neda.Darbeheshti@ga.gov.au>
2025-08-15 12:23:33 +0100

    running plots for reprts

--- runPlots/RunPlotFinalMatMosaic.m
+++ runPlots/RunPlotFinalMatMosaic.m
@@ -39,16 +39,13 @@ GRID_PARA.filterSize=15;% filter size for spatial grid weight, this value is fro
 GRID_PARA.filterRadius=10; % filter radius for spatial grid weight, this value is from experiment for tiles of one degree
 % Grid extents - ensure these values are in GRID_PARA.STEP degree value increments.
 % Boundary for computation
-% VicNSW=[140 154 -37.5 -27.5];
-% NENSW=[153 154 -29 -28];
-% vic=[140 154 -39 -33];
-% NSW=[140 154 -38 -27];
-%[93 174 -61 -8];
-%vicAdel=[137 154 -40 -33]
-GRID_PARA.MINLONG=137;%140;%110;%140;
-GRID_PARA.MAXLONG=140;%154;%160;%154;
-GRID_PARA.MINLAT=-36;%%-39;%-37.5;
-GRID_PARA.MAXLAT=-33.5;%-33;%-27.5;
+%Adelaid=[137 140 -36 -33.5]
+%Victoria=[141 150 -39 -34]
+%NSW=[141 153 -37 -29]
+GRID_PARA.MINLONG=137;
+GRID_PARA.MAXLONG=140;
+GRID_PARA.MINLAT=-36;
+GRID_PARA.MAXLAT=-33.5;
 %% DEM data - N.B. the dem is used to specify the grid nodes.
 DEM_PARA.filename='Data/DEM/AUSDEM1min.xyz';
 DEM_PARA.num_cols=4861;
@@ -94,7 +91,7 @@ GGM_PARA.filename='Data/GGM/GOCE_For_Gridded_Int.mat';%'Data/GGM/EGM2008_For_Gri
 COAST_PARA.filename='Data/COASTLINE/CoastAus.mat';
 %% Levelling data comparisons
 LEVELLING_PARA.Lev_eval=true;% If true, the levelling data are compared to the geoid as its computed.
-LEVELLING_PARA.filename='Data/GPS_LEVELLING/Lev3_92pointsSouthNEXY.mat';%8749 points,'Data/GPS_LEVELLING/Lev_CARS.mat';% The format of these data needs to be an array with rows [Long,Lat,h-H].
+LEVELLING_PARA.filename='Data/GPS_LEVELLING/CARS2009zeta7506.mat';%8749 points,'Data/GPS_LEVELLING/Lev_CARS.mat';% The format of these data needs to be an array with rows [Long,Lat,h-H].
 LEVELLING_PARA.Plot_Stats=false;% If true, the levelling data are compared to the geoid as its computed.
 LEVELLING_PARA.Compare_To_Existing_Model=true;% If true, the levelling data are also compared to another existing geoid as its computed.
 LEVELLING_PARA.Existing_Model='Data/EXISTING_GEOID_MODELS/AGQG20221120.mat';% File location of the existing model.
@@ -106,9 +103,18 @@ OUTPUT_PARA.Grids_name=['outputs/Grids',outputName,'/'];
 OUTPUT_PARA.PLOT_GRIDS=true;% A gridded solution is plotted and output as well as the tiles.
 OUTPUT_PARA.plotsFolder=['outputs/Grids',outputName,'/',plotName];
 % If there is a region of interest, for plotting purposes
-OUTPUT_PARA.polygonLon =[137.5 137.5 139.5 139.5 137.5];%[144 144 150.5 150.5 144]; %[144.3 144.3 145.2 145.2 144.3];%[147.4 147.4 147.6 147.6 147.4];%marsden%otway[141 141 143 143 141];
-OUTPUT_PARA.polygonLat =[-34 -35.5 -35.5 -34 -34];%[-35.5 -39.5 -39.5 -35.5 -35.5]; %[-37.7 -38.5 -38.5 -37.7 -37.7];%[-33.4 -33.6 -33.6 -33.4 -33.4];%marsden%otway[-37 -38.5 -39 -37.5 -37];
-
+AdelaidLon=[137.5 137.5 139.5 139.5 137.5];
+AdelaidLat=[-34 -35.5 -35.5 -34 -34];
+%VicLon=[144.5 144.5 150 150 144.5];
+%VicLat=[-36.5 -39 -39 -36.5 -36.5];
+%otwayLon=[141 141 143 143 141];
+%otwayLat=[-37 -38.5 -39 -37.5 -37];
+%NSWinLon=[141.5 141.5 147 147 141.5];
+%NSWinLat=[-29.5 -34 -34 -29.5 -29.5];
+%NSWoutLon=[150.5 148 150.5 152.5 150.5];
+%NSWoutLat=[-29.5 -35.5 -35.5 -29.5 -29.5];
+OUTPUT_PARA.polygonLon = AdelaidLon;
+OUTPUT_PARA.polygonLat = AdelaidLat;
 % Keep the computer awake
 keepawake=true;% Setting this to true wiggles the mouse every so often so the compute doesnt go to sleep.
 
@@ -135,7 +141,9 @@ if GRAV_PARA.inputGravity_weighting
      Gravo = weightInputGravity(Gravo,Coastline,GRID_PARA,OUTPUT_PARA);
 end
 
-dateCreated ='08-Aug-2025';
+% read final matfiles
+
+dateCreated ='13-Aug-2025';
 
 load([OUTPUT_PARA.Grids_name,'geomGravDiff',dateCreated,'.mat'])
 
@@ -153,23 +161,21 @@ ZDeg=mean(mean(REFERENCE_Zeta_griddedInterpolant(LongDEM,LatDEM)-GGM_Zeta_gridde
 
 resAGQG=REFERENCE_Zeta_griddedInterpolant(LongDEM,LatDEM)-GGM_Zeta_griddedInterpolant(LongDEM,-LatDEM,LatDEM*0);
 
-AGQG_Vals_Lev=Lev(:,3)-REFERENCE_Zeta_griddedInterpolant(Lev(:,1),Lev(:,2));
+% from each tile?
+Geoid_temp=double(Grid_res_geoid_w+GGM_Zeta_griddedInterpolant(LongDEM,-LatDEM,LatDEM*0));
+       
+geoidLSCgriddedInterpolant=griddedInterpolant(LongDEM(end:-1:1,:)',LatDEM(end:-1:1,:)',Geoid_temp(end:-1:1,:)');
+    
+geomGravDiff=Lev(:,3)-geoidLSCgriddedInterpolant(Lev(:,1),Lev(:,2));  
+    
+AGQG_Vals_Lev=Lev(:,3)-REFERENCE_Zeta_griddedInterpolant(Lev(:,1),Lev(:,2)); 
 
 
 plotMosaicTiles(Coastline,GRID_PARA,LongDEM,LatDEM,Grid_res_geoid_w,resAGQG,ZDeg,Lev,geomGravDiff, AGQG_Vals_Lev, ...
 Grid_res_geoid_err_w,Grid_res_grav_w,Grid_res_grav_Bouguer_w,Grid_res_grav_err_w,OUTPUT_PARA.plotsFolder)
 
+DisplayAreaStatistics(Coastline,GRID_PARA,LongDEM,LatDEM,Grid_res_geoid_w, ...
+    Grid_res_geoid_err_w,OUTPUT_PARA)
 
-
-
-% geomGravGeoidDiff = mosaicTiles(GRID_PARA,DEM_PARA,OUTPUT_PARA,Lev,LongDEM,LatDEM, ...
-%     REFERENCE_Zeta_griddedInterpolant,GGM_Gravity_griddedInterpolant,GGM_Zeta_griddedInterpolant,Coastline);
-% geomGravGeoidDiffMeanSubtracted=geomGravGeoidDiff-mean(geomGravGeoidDiff);
-% fprintf('%f length  geomGravGeoidDiff\n',length (geomGravGeoidDiffMeanSubtracted));
-% fprintf('%f min     geomGravGeoidDiff\n',min    (geomGravGeoidDiffMeanSubtracted));
-% fprintf('%f max     geomGravGeoidDiff\n',max    (geomGravGeoidDiffMeanSubtracted));
-% fprintf('%f mean    geomGravGeoidDiff\n',mean   (geomGravGeoidDiffMeanSubtracted));
-% fprintf('%f median  geomGravGeoidDiff\n',median (geomGravGeoidDiffMeanSubtracted));
-% fprintf('%f std     geomGravGeoidDiff\n',std    (geomGravGeoidDiffMeanSubtracted));
 
 diary off
--- runPreparation/CombineGPSlevellingAdelaide.m
+++ runPreparation/CombineGPSlevellingAdelaide.m
@@ -1,31 +1,68 @@
-% combine GPS-levelling data for greater Adelaide  
-figure
-scatter(Lev2(:,1),Lev2(:,2),10,Lev2(:,3))
-colorbar
-colormap(jet)
-title(colorbar,'m','FontSize',10);
-title('Lev2')
+% Specify file name and sheet (optional)
+filename = 'Data/GPS_LEVELLING/taje_a_1412353_sm4972.xls';
+sheetName = 'CARS2009';%'AHD'; 
 
-figure
-scatter(Lev3(:,1),Lev3(:,2),10,Lev3(:,3))
-colorbar
-colormap(jet)
-title(colorbar,'m','FontSize',10);
-title('Lev3')
+% Read the data
+data = readtable(filename, 'Sheet', sheetName);
 
-Lev(Lev(:,3) == 1000, :) = [];
+% Extract three specific columns by name
+% Replace 'Column1', 'Column2', 'Column3' with your actual column headers
+col1 = data.LONDD;
+col2 = data.LATDD;
+col3 = data.Zeta;
 
-% Combine data sets.
-all3GPSlevelling=[Lev(:,1),Lev(:,2),Lev(:,3);...
-                  Lev2(:,1),Lev2(:,2),Lev2(:,3);...
-                  Lev3(:,1),Lev3(:,2),Lev3(:,3)];
+% Combine into a single array if needed
+selectedData = [col1 col2 col3];
 
-figure
-scatter(all3GPSlevelling(:,1),all3GPSlevelling(:,2),10,all3GPSlevelling(:,3))
-colorbar
-colormap(jet)
-title(colorbar,'m','FontSize',10);
+%save('Data/GPS_LEVELLING/AHDzeta7506.mat','selectedData')
+save('Data/GPS_LEVELLING/CARS2009zeta7506.mat','selectedData')
 
-save('Data/GPS_LEVELLING/Lev_Adelaide.mat','all3GPSlevelling')
+
+
+
+
+
+
+
+
+
+
+
+
+% % combine GPS-levelling data for greater Adelaide  
+% figure
+% scatter(Lev2(:,1),Lev2(:,2),10,Lev2(:,3))
+% colorbar
+% colormap(jet)
+% title(colorbar,'m','FontSize',10);
+% title('Lev2')
+% 
+% figure
+% scatter(Lev3(:,1),Lev3(:,2),10,Lev3(:,3))
+% colorbar
+% colormap(jet)
+% title(colorbar,'m','FontSize',10);
+% title('Lev3')
+% 
+% Lev(Lev(:,3) == 1000, :) = [];
+% 
+% % Combine data sets.
+% all3GPSlevelling=[Lev(:,1),Lev(:,2),Lev(:,3);...
+%                   Lev2(:,1),Lev2(:,2),Lev2(:,3);...
+%                   Lev3(:,1),Lev3(:,2),Lev3(:,3)];
+% 
+% figure
+% scatter(all3GPSlevelling(:,1),all3GPSlevelling(:,2),10,all3GPSlevelling(:,3))
+% colorbar
+% colormap(jet)
+% title(colorbar,'m','FontSize',10);
+% 
+% save('Data/GPS_LEVELLING/Lev_Adelaide.mat','all3GPSlevelling')
+
+
+
+
+
+
 
 
